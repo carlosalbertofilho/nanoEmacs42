284bbfa1cc38660df18d90c7f3756e9a57fa5b7a
Carlos Alberto Filho <csilva-d@student.42.rio>
2025-10-15 15:26:01 -0300

    adicionado nano emacs

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2025 Carlos Filho
 ;;
 
-;; --- Straight.el --------------------------------------------------------
+;; --- Straight.el ------------------------------------------------------------
 ;; Instala o straight.el como gerenciador de pacotes
 (defvar bootstrap-version)
 (let ((bootstrap-file
@@ -21,4 +21,21 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
+;; --- Typography stack -------------------------------------------------------
+(set-face-attribute 'default nil
+                    :height 110 :weight 'light :family "FiraCode Nerd Font Mono")
+(set-face-attribute 'bold nil :weight 'regular)
+(set-face-attribute 'bold-italic nil :weight 'regular)
+(set-display-table-slot standard-display-table 'truncation (make-glyph-code ?…))
+(set-display-table-slot standard-display-table 'wrap (make-glyph-code ?–))
 
+
+;; --- Activate / Deactivate modes --------------------------------------------
+(tool-bar-mode -1) (menu-bar-mode -1) (blink-cursor-mode -1)
+(global-hl-line-mode 1)
+
+;; --- Nano Emacs-- -----------------------------------------------------------
+;; Instala e configura o Nano Emacs
+(straight-use-package '(nano :type git :host github :repo "rougier/nano-emacs"))
+
+(require 'nano)
